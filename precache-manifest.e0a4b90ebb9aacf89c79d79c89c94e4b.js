1d8d4c6fe45ae42cba66191b837d8b9071740f74
Alexander Kub <alexdarkalt@gmail.com>
2019-04-10 21:59:10 +0300

    Updates

---
+++ precache-manifest.e0a4b90ebb9aacf89c79d79c89c94e4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cfb905376fb6c80563c",
+    "url": "/react-test-items/static/css/main.2932e9b8.chunk.css"
+  },
+  {
+    "revision": "6cfb905376fb6c80563c",
+    "url": "/react-test-items/static/js/main.640904cf.chunk.js"
+  },
+  {
+    "revision": "8b58b0c925f46c10d36b",
+    "url": "/react-test-items/static/js/runtime~main.22352647.js"
+  },
+  {
+    "revision": "c137899a6eb5b918b3e5",
+    "url": "/react-test-items/static/js/2.18b25b4a.chunk.js"
+  },
+  {
+    "revision": "011913c90d74f49440e7527341f420fc",
+    "url": "/react-test-items/index.html"
+  }
+];
